ffb1af5ac9c3626a98eecc7f5024eadd5b9c50c1
Arm4nd7 <armando.estevezt@gmail.com>
2025-01-10 23:42:44 -0500

    laboratorio 9 - pokedex-lab8-modelosVistaControlador

--- pokedex/models.py
+++ pokedex/models.py
@@ -24,6 +24,7 @@ class Pokemon(models.Model):
     weight = models.DecimalField(decimal_places = 4, max_digits = 6)
     height = models.DecimalField(decimal_places = 4, max_digits = 6)
     trainer = models.ForeignKey(Trainer, on_delete = models.SET_NULL, null=True)
+    picture = models.ImageField(upload_to="pokemons_images")
     
     def __str__(self):
         return self.name
